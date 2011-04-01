1f6973e43e7d4a3ddc9db5a2b645f0fbb5d4c699
Gabriel Farrell <gsf747@gmail.com>
2011-04-01 13:38:52 -0400

    Pointed test-count at new file places

--- test/test-count.js
+++ test/test-count.js
@@ -1,13 +1,5 @@
-var marc = require('./marc');
+var marc = require('../marc');
 
-marc.count('test_set.utf8.mrc', function(count) {
+marc.count('test/books.mrc', function(count) {
   console.log('Count: '+count);
 });
-
-//marc.map('art.utf8.mrc', function(recordBuffer) {
-//  var leaderBuffer = marc.getLeaderBuffer(recordBuffer);
-//  //console.log(leaderBuffer.toString());
-//  var directoryBuffer = marc.getDirectoryBuffer(recordBuffer, 
-//    marc.getBaseAddress(leaderBuffer));
-//  //console.log(directoryBuffer.toString());
-//});
